3b3242e6d859e5e2f58899bfdbc5f07d7c0ec65b
Kennytian <kenny_tian@outlook.com>
2018-04-23 16:44:39 +0800

    Refactor and format code

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-import { query } from './async-mysql';
+import { query } from '../utils/mysql';
 import { md5 } from '../utils/crypto';
 
 const index = async (ctx) => {
-  ctx.response.body = `<h1>Index${ctx.session.realName || ''}</h1>
+  ctx.response.body = `<h1>Index-${ctx.session.realName || ''}</h1>
         <form action="/signIn" method="post">
             <p>Name: <input name="name" value="admin"></p>
             <p>Password: <input name="password" type="password" value="123456"></p>
@@ -17,12 +17,10 @@ const signIn = async (ctx) => {
   const sql = `select * from admin where username = '${name}' and password = '${password}';`;
 
   const result = await query(sql);
-  console.log(result);
   const { username, realname } = result[0];
   if (username === 'admin' && realname === 'Kenny') {
     ctx.session.realName = realname;
-    console.log('username, realname, ctx.session:', username, realname, ctx.session);
-    ctx.response.body = `<h1>Welcome, ${realname}!</h1><h2><a href="../blog/kenny">blog/kenny</a></h2>`;
+    ctx.response.body = `<h1>Welcome, ${ctx.session.realName}!</h1><h2><a href="../blog/kenny">blog/kenny</a></h2>`;
   } else {
     ctx.response.body = '<h1>Login failed!</h1><p><a href="/">Try again</a></p>';
   }
