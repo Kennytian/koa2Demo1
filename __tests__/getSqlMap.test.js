e22867a13be32fbfc337d61d1e38016d35c167af
Kennytian <kenny_tian@outlook.com>
2018-04-26 10:01:29 +0800

    Fix unit test code

--- __tests__/getSqlMap.test.js
+++ __tests__/getSqlMap.test.js
@@ -1,12 +1,19 @@
 /* eslint-disable no-undef,max-len */
 import getSqlMap from '../src/utils/getSqlMap';
+import { ROOT_PATH } from '../src/const/site';
 
 describe('getSqlMap group', () => {
   it('getSqlMap not.toBeNull', () => {
-    expect(getSqlMap()).not.toBeNull();
+    expect(getSqlMap()).toBeDefined();
   });
 
-  it('getSqlMap can work', () => {
-    expect(getSqlMap()).toEqual(1);
+  it('getSqlMap get sql path', () => {
+    expect(getSqlMap()).toEqual({ 'admin.sql': '/Users/kenny/projects/private/nodejs/koa2Demo1/src/sql/admin.sql' });
+  });
+
+  it('getSqlMap ROOT_PATH', () => {
+    expect(__dirname).toContain('/__tests__');
+    expect(ROOT_PATH).not.toContain('/__tests__');
+    expect(ROOT_PATH).toBe('/Users/kenny/projects/private/nodejs/koa2Demo1');
   });
 });
--- src/utils/getSqlMap.js
+++ src/utils/getSqlMap.js
@@ -2,7 +2,7 @@ import walkFile from './walkFile';
 import { ROOT_PATH } from '../const/site';
 
 export default function getSqlMap() {
-  const path = `${ROOT_PATH}/../src/sql/`;
+  const path = `${ROOT_PATH}/src/sql/`;
   const fileList = walkFile(path, 'sql');
   return fileList;
 }
